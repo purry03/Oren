7b6a761f13b1627f53711078de55386164db6d30
rohan.sharma <rohan.sharma@zeeve.io>
2023-03-25 23:18:37 +0530

    Added response object

--- public/js/user/index.js
+++ public/js/user/index.js
@@ -1,20 +1,81 @@
-// type 3 question - add button
+const response = {};
 
+// textarea event listener
+
+function calculateResponse(textarea) {
+	const questionElement = $(textarea).closest('div').find('[questionId]');
+	const type = questionElement.attr('class');
+	const questionId = questionElement.attr('questionId');
+	switch(type){
+	case 'type1':
+		const value = $(textarea).val();
+		response[questionId] = value;
+		break;
+	case 'type2':
+		// iterate through all textareas in question Element
+		response[questionId] = [];
+		$('textarea', $(questionElement)).each(function(){
+			const value = $(this).val();
+			response[questionId].push(value);
+		});
+		break;
+	case 'type3':
+		// iterate through all textareas in question Element
+		response[questionId] = [];
+		$('textarea', $(questionElement)).each(function(){
+			const value = $(this).val();
+			response[questionId].push(value);
+		});
+		break;
+	case 'type4':
+		// iterate through all textareas in question Element
+		response[questionId] = [];
+		$('textarea,select', $(questionElement)).each(function(){
+			const value = $(this).val();
+			response[questionId].push(value);
+		});
+		break;
+	}
+}
+
+/*
+* when the page is loaded
+* scan through all textarea in the body
+* and call calculateResponse() for them
+* this is so that the response object is
+* intialized with a default value
+*/
+document.addEventListener('DOMContentLoaded', function() {
+	$('textarea', $(document.body)).each(function () {
+		calculateResponse(this);
+	});	
+}, false);
+
+
+/*
+* whenever a textarea in the document is changed
+* call calculateResponse()
+*/
+$(document.body).on('input','textarea', function (e) {
+	calculateResponse(this);
+});
+
+// type 3 question - add button
 $(document.body).on('click','.add-row-button', function (e) {
 	const table = $(this).closest('table');
 	let elementCount = parseInt(table.attr('elements'));	// get current element count
 	table.attr('elements', elementCount += 1);	// increment element count by one
-	table.children('tbody').append(`<tr>
+	table.children('tbody').append(`
+	<tr>
 	<th class="sno">${elementCount}</th>
 	<td><textarea></textarea></td>
 	<th class="delete-row-button"><i class="fa-regular fa-circle-xmark"></i></th>
-	</tr>`);
+	</tr>
+	`);
 });
 
 // type 3 qeuestion - delete button
-
 $(document.body).on('click','.delete-row-button' ,function (e) {
-	console.log($(this));
 	const table = $(this).closest('table');
 	let elementCount = parseInt(table.attr('elements'));	// get current element count
 	$(this).closest('tr').remove();
@@ -25,4 +86,7 @@ $(document.body).on('click','.delete-row-button' ,function (e) {
 		$(this).html(idx);
 		idx += 1;
 	});
+	// recalcuate response
+	const childTextarea = table.find('textarea');	// get any child textarea of the table
+	calculateResponse(childTextarea);
 });
